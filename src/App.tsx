efc2e2f9d676fbf559a964c0ea2e113af2262f18
Pratyush Tewari <tewari.pratyush@gmail.com>
2023-08-23 13:18:44 -0400

    padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { App, Button, Card, ConfigProvider, Space, theme } from "antd";
+import { App, Button, Card, ConfigProvider, theme } from "antd";
 import MaterialSymbol from "./component/MaterialSymbol";
 
 // Sub page
@@ -12,23 +12,26 @@ const MyPage = () => {
           color: token.colorText,
           background: token.colorBgLayout,
           border: "2px solid green",
+          padding: token.padding,
         }}
       >
+        {" "}
+        <h3 style={{ color: token.colorWarningText }}>
+          The backgroundColor of this div green border which is set to
+          <span>
+            <pre>token.colorBgLayout</pre>
+          </span>
+          should be dark in dark mode.
+        </h3>
         <Card>
           <p>this is a card</p>
           <Button type="primary" icon={<MaterialSymbol name="psychiatry" />}>
             psychiatry
           </Button>
         </Card>
-        <h1 style={{ color: token.colorWarningText }}>
-          The backgroundColor of this div which is set to
-          <span>
-            <pre>token.colorBgLayout</pre>
-          </span>
-          should be dark in dark mode.
-        </h1>
-        <h1> Now lets override it using a nested ConfigProvider</h1>
+        <h3> Now lets override it using a nested ConfigProvider</h3>
       </div>
+      <br></br>
       <ConfigProvider
         theme={{
           algorithm: theme.defaultAlgorithm,
@@ -39,21 +42,23 @@ const MyPage = () => {
             color: token.colorText,
             background: token.colorBgLayout,
             border: "2px solid red",
+            padding: token.padding,
           }}
         >
+          <h3 style={{ color: token.colorWarningText }}>
+            The backgroundColor of this div which is set to
+            <span>
+              <pre>token.colorBgLayout</pre>
+            </span>
+            should be light and in light mode.
+          </h3>
           <Card>
             <p>this is a card</p>
             <Button type="primary" icon={<MaterialSymbol name="psychiatry" />}>
               psychiatry
             </Button>
           </Card>
-          <h1 style={{ color: token.colorWarningText }}>
-            The backgroundColor of this div which is set to
-            <span>
-              <pre>token.colorBgLayout</pre>
-            </span>
-            should be light and in light mode.
-          </h1>
+          <p> the card actually get overriden, but not the token that is applied directly to the div</p>
         </div>
       </ConfigProvider>
     </App>
